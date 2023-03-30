bb70a44fa4d584ed1614d99314a38555c2c5f48c
Evangelos Zigkas <82516621+EvanZig@users.noreply.github.com>
2023-03-30 12:25:51 +0300

    test

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,32 @@
 import React from 'react'
+import formik from "formik"
+import { CForm, CFormCheck, CFormLabel,CButton,CFormInput,CFormText } from '@coreui/react'
+import '@coreui/coreui/dist/css/coreui.min.css';
 
 export default function App() {
 
 
   return (
-    <>
-    <h1>yooo</h1>
-    </>
+    <CForm>
+    <div className="mb-3">
+    <CFormLabel htmlFor="exampleInputEmail1">Email address</CFormLabel>
+    <CFormInput type="email" id="exampleInputEmail1" aria-describedby="emailHelp" />
+    <CFormText id="emailHelp">We'll never share your email with anyone else.</CFormText>
+  </div>
+  <div className="mb-3">
+    <CFormLabel htmlFor="exampleInputPassword1">Email Password</CFormLabel>
+    <CFormInput type="password" id="exampleInputPassword1" />
+  </div>
+  <CFormCheck
+    className="mb-3"
+    label="Check me out"
+    onChange={(e) => {
+      console.log(e.target)
+    }}
+  />
+  <CButton type="submit" color="primary">
+    Submit
+  </CButton>
+</CForm>
   )
 }
