c8f114c6615d0781e78a300ae3856b414c689a48
Evangelos Zigkas <82516621+EvanZig@users.noreply.github.com>
2023-03-30 13:16:47 +0300

    test

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import formik from "formik"
-import { CForm, CFormCheck, CFormLabel,CButton,CFormInput,CFormText } from '@coreui/react'
+import { CForm, CFormLabel,CButton,CFormInput,CFormText,CInputGroup } from '@coreui/react'
 import '@coreui/coreui/dist/css/coreui.min.css';
 
 export default function App() {
@@ -8,25 +8,30 @@ export default function App() {
 
   return (
     <CForm>
-    <div className="mb-3">
-    <CFormLabel htmlFor="exampleInputEmail1">Email address</CFormLabel>
-    <CFormInput type="email" id="exampleInputEmail1" aria-describedby="emailHelp" />
-    <CFormText id="emailHelp">We'll never share your email with anyone else.</CFormText>
-  </div>
-  <div className="mb-3">
-    <CFormLabel htmlFor="exampleInputPassword1">Email Password</CFormLabel>
-    <CFormInput type="password" id="exampleInputPassword1" />
-  </div>
-  <CFormCheck
-    className="mb-3"
-    label="Check me out"
-    onChange={(e) => {
-      console.log(e.target)
-    }}
-  />
-  <CButton type="submit" color="primary">
-    Submit
-  </CButton>
-</CForm>
+
+      <CInputGroup className="mb-4">
+      <CFormInput
+      placeholder="First name"
+      className="light-background"
+      />
+      </CInputGroup>
+
+      <CInputGroup className="mb-4">
+      <CFormInput
+      placeholder="Last name"
+      className="light-background"
+      />
+      </CInputGroup>
+
+      <CInputGroup className="mb-4">
+      <CFormInput
+      type="email"
+      placeholder= "E-mail"
+      className="light-background"
+      />
+      </CInputGroup>
+
+    </CForm>
+
   )
 }
