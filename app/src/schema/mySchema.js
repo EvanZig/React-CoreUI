bafd53a6382c0973364e86f26f2fa9a3780c2772
GitSkrud <euaggeloszigas@gmail.com>
2023-04-03 11:26:41 +0300

    finalised

--- app/src/schema/mySchema.js
+++ app/src/schema/mySchema.js
@@ -1,12 +1,12 @@
 import * as yup from "yup"
 
 const passwordSchema = /^(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?])(?=.*[0-9])(?=.*[A-Z]).{8,}$/
+const passwordError = "eight characters minimum \nonespecialcharacter(e.g !,%,@,$,#,^, . , ...\none number\none uppercase character"
 
 export const mySchema = yup.object().shape({
     email: yup.string().email().required(),
-    password: yup.string().min(8, "eight characters minimum \nonespecialcharacter(e.g !,%,@,$,#,^,.,...\none number\none uppercase character").required(),
+    password: yup.string().min(8).matches(passwordSchema, {message: passwordError}).required(),
     confirmPassword: yup.string().oneOf([yup.ref('password'), null], "Passwords must match").required(),
 })
 
-const errors = "eight characters minimum \nonespecialcharacter(e.g !,%,@,$,#,^, . , ...\none number\none uppercase character"
-export const passwordErrorsArray = errors.split("\n")
+export const passwordErrorsArray = passwordError.split("\n")
